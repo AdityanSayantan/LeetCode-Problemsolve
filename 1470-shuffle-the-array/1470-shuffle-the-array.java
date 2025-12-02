0bc416f0e1b8c5a00b4fb95d5054130a9f05ec08
AdityanSayantan <23p31a0551@acet.ac.in>
2025-12-03 00:15:36 +0530

    Runtime 0 ms Beats 100.00% Analyze Complexity Memory 46.65 MB Beats 10.55%

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -8,3 +8,19 @@ class Solution {
         return result;
     }
 }
+
+
+// nums = [2, 5, 1, 3, 4, 7]
+// n = 3
+// result = new int[6] // creates [0, 0, 0, 0, 0, 0]
+// result[2 * i] = nums[i];
+// // 2 * 0 = 0
+// // result[0] = nums[0] = 2
+// // result becomes: [2, 0, 0, 0, 0, 0]
+
+// result[2 * i + 1] = nums[n + i];
+// // 2 * 0 + 1 = 1
+// // n + i = 3 + 0 = 3
+// // result[1] = nums[3] = 3
+// // result becomes: [2, 3, 0, 0, 0, 0]
+// Remaining as it is
