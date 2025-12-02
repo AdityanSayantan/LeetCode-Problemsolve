142991081e2964f3df804420fd140e016d10ac64
AdityanSayantan <23p31a0551@acet.ac.in>
2025-12-02 08:53:08 +0530

    Update 0169-majority-element.java

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,19 +3,15 @@ class Solution {
         int count = 0;
         int major = 0;
         for(int i=0;i<nums.length;i++){
-            if( major == nums[i]){
-                count++;
-
-            }else if(count==0){
+            if(count==0){
                 major = nums[i];
                 count++;
-            }
-            else{
+            }else if( major == nums[i]){
+                count++;
+            } else{
                 count--;
             }  
         }
-     
         return major;
-
     }
 }
