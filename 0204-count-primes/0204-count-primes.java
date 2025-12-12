aa3a16458cdf196aad1fd54687d5abb006c11d6e
AdityanSayantan <23p31a0551@acet.ac.in>
2025-12-12 09:15:20 +0530

    Time: 100 ms (44.77%), Space: 51 MB (43.11%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,21 +1,38 @@
 class Solution {
     public int countPrimes(int n) {
+     // Edge case: no primes less than 2
         if (n <= 2) {
-        return 0;
-    }
-
-    int[] isCheck = new int[n];
-    int count = 0;
-
-    for (int i = 2; i < n; i++) {
-        if (isCheck[i] == 0) {
-            count++;
-            for (int j = 2 * i; j < n; j += i) {
-                isCheck[j] = 1;
+            return 0;
+        }
+      
+        // Initialize boolean array to track prime status
+        // isPrime[i] represents whether number i is prime
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+      
+        // 0 and 1 are not prime numbers
+        isPrime[0] = false;
+        isPrime[1] = false;
+      
+        int primeCount = 0;
+      
+        // Sieve of Eratosthenes: iterate through numbers starting from 2
+        for (int i = 2; i < n; i++) {
+            // If current number is prime
+            if (isPrime[i]) {
+                primeCount++;
+              
+                // Mark all multiples of i as non-prime
+                // Optimization: start from i * i instead of i + i
+                if ((long) i * i < n) {
+                    for (int j = i * i; j < n; j += i) {
+                        isPrime[j] = false;
+                    }
+                }
             }
         }
+      
+        return primeCount;  
     }
-
-    return count;
-    }
+    
 }
