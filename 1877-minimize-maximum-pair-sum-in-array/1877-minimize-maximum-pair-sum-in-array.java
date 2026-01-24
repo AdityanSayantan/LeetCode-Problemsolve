ba3f985333aa78682867f9594f6c612abd966377
AdityanSayantan <23p31a0551@acet.ac.in>
2026-01-24 19:37:22 +0530

    Time: 10 ms (96.71%), Space: 98.3 MB (5.16%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -1,21 +1,33 @@
 class Solution {
     public int minPairSum(int[] nums) {
-        Arrays.sort(nums);
+        int maxVal = 0;
+        for (int num : nums) {
+            maxVal = Math.max(maxVal, num);
+        }
         
-        int n = nums.length;
-        int maxSum = 0;
+        int[] count = new int[maxVal + 1];
+        for (int num : nums) {
+            count[num]++;
+        }
         
-        // Pair the smallest with the largest, second smallest with second largest, etc.
-        for (int i = 0; i < n/2; i++) {
-            // Pair nums[i] (small elements) with nums[n-1-i] (large elements)
-            int pairSum = nums[i] + nums[n - 1 - i];
-            
-            // Keep track of the maximum pair sum
-            if (pairSum > maxSum) {
-                maxSum = pairSum;
+        // Reconstruct sorted array
+        int idx = 0;
+        for (int i = 0; i <= maxVal; i++) {
+            while (count[i]-- > 0) {
+                nums[idx++] = i;
             }
         }
         
-        return maxSum;
+        // Two-pointer approach for pairing
+        int maxPairSum = 0;
+        int left = 0, right = nums.length - 1;
+        
+        while (left < right) {
+            maxPairSum = Math.max(maxPairSum, nums[left] + nums[right]);
+            left++;
+            right--;
+        }
+        
+        return maxPairSum;
     }
 }
